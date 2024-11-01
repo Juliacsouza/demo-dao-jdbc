92398e0759055386c469247fe404f1cda9940735
Juliacsouza <juliacsouza573@gmail.com>
2024-10-31 23:49:54 -0300

    delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -65,12 +65,10 @@ public class SellerDaoJDBC implements SellerDao {
 
 		PreparedStatement st = null;
 		try {
-			st = conn.prepareStatement(
-					"UPDATE seller "
-					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
-					+ "WHERE Id = ?");
-																						// retornar o id do novo
-																						// vendedir inserido.
+			st = conn.prepareStatement("UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " + "WHERE Id = ?");
+			// retornar o id do novo
+			// vendedir inserido.
 
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getName());
@@ -79,7 +77,7 @@ public class SellerDaoJDBC implements SellerDao {
 			st.setInt(5, obj.getDepartment().getId());
 			st.setInt(6, obj.getId());
 
-		    st.executeUpdate();
+			st.executeUpdate();
 
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
@@ -91,8 +89,17 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller\r\n" + "WHERE Id = ?");
+			st.setInt(1, id);
 
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
